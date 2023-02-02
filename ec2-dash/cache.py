98e0a8c1c23ee5be4092309fd6f39e7404e9448d
Dominic Lawson <dominic.l12@hotmail.co.uk>
2023-02-02 16:41:44 +0000

    redis cache to reduce user email spam

---
+++ ec2-dash/cache.py
@@ -0,0 +1,26 @@
+import redis
+
+redisClient = redis.Redis(host='dominicpharmacache.c1dlpv.ng.0001.euw2.cache.amazonaws.com', port=6379)
+# redis hashes, on for contries and one for institutes
+
+def check_cache_value(email: str) -> bool | None:
+	"""
+	Check cache for user_email
+
+	Args:
+			email (str): user email
+
+	Returns:
+			bool: True if email in cache
+	"""
+	if redisClient.get(email):
+		return True
+
+def set_cache_value(email: str) -> None:
+	"""
+	set cache value to contain user email
+
+	Args:
+			email (str): user email
+	"""
+	redisClient.set(email, True, ex=15)

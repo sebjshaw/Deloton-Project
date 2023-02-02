d644ae81c8cc83d75bfde4ea0173f4fd40d7db42
SebShaw <trainee.seb.shaw@sigmalabs.co.uk>
2023-02-02 10:30:25 +0000

    Remove join table. Add max_hr to user_info table
    Removed the join table as the user_id and ride_id are both now in every log of the current ride table. Max_hr has been added to the user_info table to be used in the dashboard

--- ec2-dash/dash_sqlite.py
+++ ec2-dash/dash_sqlite.py
@@ -31,6 +31,8 @@ def create_new_current_ride_table(cursor: sqlite3.Cursor, conn: sqlite3.Connecti
     # creates a new current_ride table 
     cursor.execute("""
     CREATE TABLE current_ride (
+        user_id TEXT,
+        ride_id TEXT,
         date TEXT, 
         time TEXT,
         duration INTEGER NOT NULL,
@@ -43,7 +45,7 @@ def create_new_current_ride_table(cursor: sqlite3.Cursor, conn: sqlite3.Connecti
 
     conn.commit()
 
-def add_entry_to_table(cursor: sqlite3.Cursor,conn: sqlite3.Connection, entry: dict):
+def add_entry_to_table(cursor: sqlite3.Cursor,conn: sqlite3.Connection, entry: dict, ride_id: int, user_id: str):
     """Adds an entry to the current_ride table. Called every time a new log is received 
     from the kafka consumer
 
@@ -51,18 +53,20 @@ def add_entry_to_table(cursor: sqlite3.Cursor,conn: sqlite3.Connection, entry: d
         cursor (sqlite3.Cursor): The cursor that allows queries to be made to the database
         conn (sqlite3.Connection): Enables the query to be committed
         entry (dict): The combined log entry for this event
+        ride_id (int): Adds the ride_id to each row of the table
+        user_id (str): Adds the user_id to each row of the table 
     """
 
     # query sent to the table current_ride to insert into table 
     cursor.execute(f"""
     INSERT INTO current_ride (
-        date, time, duration, resistance, heart_rate, rpm, power
+        user_id, ride_id, date, time, duration, resistance, heart_rate, rpm, power
     )
     VALUES (
-        "{entry['date']}", "{entry['time']}", 
-        {entry['duration']}, {entry['resistance']}, 
-        {entry['heart_rate']}, {entry['rpm']}, 
-        "{entry['power']}"
+        "{user_id}", "{ride_id}", "{entry['date']}", 
+        "{entry['time']}", {entry['duration']}, 
+        {entry['resistance']}, {entry['heart_rate']}, 
+        {entry['rpm']}, "{entry['power']}"
     )
     """)
 
@@ -86,14 +90,14 @@ def most_recent_ride_to_csv(cursor: sqlite3.Cursor):
         csv_f.writerow(['ride_id', 'date', 'time', 'duration', 'resistance', 'heart_rate', 'rpm', 'power'])
         csv_f.writerows(most_recent_ride)
 
-def create_user_info_table(cursor: sqlite3.Cursor, conn: sqlite3.Connection, user_info: dict):
-
+def recreate_user_info_table(cursor: sqlite3.Cursor, conn: sqlite3.Connection, user_info: dict, max_hr: int):
     """Deletes previous user table, creates a new one and inserts the user information
 
     Args:
         cursor (sqlite3.Cursor): The cursor that allows queries to be made to the database
         conn (sqlite3.Connection): Enables the query to be committed
         user_info (dict): The whole user information for the upcoming ride
+        max_hr (int): The max_hr is also included in the user info table
     """
 
     cursor.execute("DROP TABLE IF EXISTS user_info") #deletes the old user_info table
@@ -111,21 +115,24 @@ def create_user_info_table(cursor: sqlite3.Cursor, conn: sqlite3.Connection, use
         weight_kg INTEGER NOT NULL,
         account_create_date TEXT NOT NULL,
         bike_serial TEXT,
-        original_source TEXT
-    );
+        original_source TEXT,
+        max_hr INTEGER
+    )
+    """)
+    conn.commit()
 
     INSERT INTO user_info (
         user_id, name, gender, 
         address, date_of_birth, 
         email_address, height_cm, 
         weight_kg, account_create_date, 
-        bike_serial, original_source
+        bike_serial, original_source, max_hr
     )
     VALUES ({user_info['user_id']}, "{user_info['name']}", "{user_info['gender']}", 
     "{user_info['address']}", "{user_info['date_of_birth']}", 
     "{user_info['email_address']}", {user_info['height_cm']},
     {user_info['weight_kg']}, "{user_info['account_create_date']}",
-    "{user_info['bike_serial']}", "{user_info['original_source']}"
+    "{user_info['bike_serial']}", "{user_info['original_source']}", {max_hr}
     )
     """)
 
@@ -172,11 +179,12 @@ def push_to_s3():
     s3.upload_file('ec2-dash/most_recent_ride.csv', 'three-m-deloton-bucket', 'most_recent_ride')
     s3.upload_file('ec2-dash/user_info.csv', 'three-m-deloton-bucket', 'user_info')
 
+
 if __name__ == "__main__":
     recreate_current_ride_table(cursor, conn) #creates a table for the current ride data to be inserted into 
     ride_id = 'N/A' #placeholder for ride_id until user info is received
     ride_date = 'N/A' #placeholder until user info is received
-    recreate_join_csv('N/A', 'N/A') #placeholder for the join_csv until user info is received
+    user_id = 'N/A'
     max_hr = 220
     
     # constantly retrieving logs and creating tables and csvs
@@ -194,24 +202,25 @@ if __name__ == "__main__":
                 # delete expired csv files 
                 os.remove("ec2-dash/most_recent_ride.csv") 
                 os.remove("ec2-dash/user_info.csv") 
-                os.remove("ec2-dash/join_ids.csv") 
-
-                #regenerate the tables
-                recreate_current_ride_table(cursor, conn)
-                recreate_user_info_table(cursor, conn, log_entry)
-
-                # regenerate the join_ids and user_info csvs
-                ride_id = recreate_ride_id_from_datetime(log_entry)
-                ride_date = log_entry['date'] + " " + log_entry['time']
-                recreate_join_csv(log_entry['user_id'], ride_id)
 
                 # set new max_hr
                 # max_hr = calculate_max_heart_rate(log_entry['date_of_birth'])
+
+                #regenerate the tables
+                recreate_current_ride_table(cursor, conn)
+                recreate_user_info_table(cursor, conn, log_entry, max_hr)
+
+                # regenerate the ride_id, user_id and ride_date
+                ride_id = recreate_ride_id_from_datetime(log_entry)
+                user_id = log_entry['user_id']
+                ride_date = log_entry['date'] + " " + log_entry['time']
+                
+                
                 
 
             # only adds to the database if it is a full entry      
             if len(log_entry) == 7:
-                add_entry_to_table(cursor, conn, log_entry, ride_id)
+                add_entry_to_table(cursor, conn, log_entry, ride_id, user_id)
                 # if compare_hr_to_max_hr(log_entry['heart_rate'], max_hr) == True:
                     # send_user_info_to_lambda(log_entry['heart_rate'], log_entry['duration'])
 

0e0ccfe19fe5d49703c45081521d2da5a1565223
Dominic Lawson <dominic.l12@hotmail.co.uk>
2023-02-02 12:22:55 +0000

    callback to display name age gender complete

--- dash/visualisations.py
+++ dash/visualisations.py
@@ -1,6 +1,10 @@
 import plotly
 import plotly.express as px
+import plotly.graph_objs as go
 import pandas as pd
+from SQLConnection import SQLConnection
+
+sql = SQLConnection('./ec2-dash/dash_db.db')
 
 def create_visualisation(df: pd.DataFrame, x: str, y:str) -> px.line:
 	"""
@@ -15,9 +19,16 @@ def create_visualisation(df: pd.DataFrame, x: str, y:str) -> px.line:
 			px.line: line graph plotting x and y
 	"""
 	fig = px.line(df, x, y).update_layout(paper_bgcolor="#0d1f22", plot_bgcolor='#0d1f22')
+
 	fig.update_xaxes(title_font=dict(color='#f3dfc1'), tickfont=dict(color='#f3dfc1'))
 	fig.update_xaxes(gridcolor='#8d5b4c', zerolinecolor='#8d5b4c', zerolinewidth=3)
 	fig.update_yaxes(title_font=dict(color='#f3dfc1'), tickfont=dict(color='#f3dfc1'))
 	fig.update_yaxes(gridcolor='#8d5b4c', zerolinecolor='#8d5b4c', zerolinewidth=3)
+
+	if "heart_rate" in df.columns.to_list():
+		max_hr = sql.get_list("SELECT max_hr FROM user_info")
+		x_values = sql.get_df("SELECT duration FROM current_ride")
+
+		fig.add_trace(go.Scatter(x=df.duration, y=max_hr, mode='lines', line_color="red")) # Add line for max heart rate
 	return fig
 

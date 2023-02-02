f56c27d26ae4d34b3fcc59637012daf7208f5d40
Dominic Lawson <dominic.l12@hotmail.co.uk>
2023-02-02 17:34:09 +0000

    added api endpoint code to fetch data

--- api/api.py
+++ api/api.py
@@ -9,6 +9,7 @@ dotenv.load_dotenv()
 
 app = Flask(__name__)
 CORS(app, origins=["http://127.0.0.1:8090"],  supports_credentials=True)
+CORS(app, origins=["http://127.0.0.1:8090"],  supports_credentials=True)
 
 USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
@@ -50,6 +51,38 @@ def tuple_to_dict(tup:tuple, table:str) -> dict:
 		row_dict[title] = tup[idx]
 	return row_dict
 
+def users_tuple_to_dict(user_info:tuple) -> dict:
+	return {
+		'user_id':user_info[0],
+		'first_name':user_info[1],
+    'last_name':user_info[2],
+    'gender':user_info[3],
+		'address':user_info[4],
+		'date_of_birth':user_info[5],
+		'email_address':user_info[6],
+		'height_cm':user_info[7],
+		'weight_kg':user_info[8],
+		'account_create_date':user_info[9],
+		'bike_serial':user_info[10],
+		'original_source':user_info[11]
+	}
+
+def users_tuple_to_dict(user_info:tuple) -> dict:
+	return {
+		'user_id':user_info[0],
+		'first_name':user_info[1],
+    'last_name':user_info[2],
+    'gender':user_info[3],
+		'address':user_info[4],
+		'date_of_birth':user_info[5],
+		'email_address':user_info[6],
+		'height_cm':user_info[7],
+		'weight_kg':user_info[8],
+		'account_create_date':user_info[9],
+		'bike_serial':user_info[10],
+		'original_source':user_info[11]
+	}
+
 # # GET /ride/:id
 # Get a ride with a specific ID
 @app.route("/ride/<int:ride_id>", methods=["GET"])
@@ -59,16 +92,13 @@ def get_ride_info(ride_id:int):
 			f"""
 				SELECT *
 				FROM rides
-				WHERE rides_id = {ride_id}
+				WHERE ride_id = {ride_id}
 			"""
 		)[0]
-		if ride_info_tuple:
-			ride_info = tuple_to_dict(ride_info_tuple, 'rides')
-			return jsonify(ride_info)
-		else:
-			return jsonify({"output": 'No matching records'})
 	except Exception as e:
-		return jsonify({'error':e})
+		return jsonify(e)
+	ride_info = rides_tuple_to_dict(ride_info_tuple)
+	return jsonify(ride_info)
 
 # # GET /rider/:user_id
 # Get rider information (e.g. name, gender, age, avg. heart rate, number of rides)
@@ -82,18 +112,15 @@ def get_user_info(user_id:int):
 				WHERE user_id = {user_id}
 			"""
 		)[0]
-		if user_info_tuple:
-			user_info = tuple_to_dict(user_info_tuple, 'users')
-			return jsonify(user_info)
-		else:
-			return jsonify({"output": 'No matching records'})
 	except Exception as e:
-		return jsonify({'error':e})
+		return jsonify(e)
+	user_info = users_tuple_to_dict(user_info_tuple)
+	return jsonify(user_info)
 
 # # GET /rider/:user_id/rides
 # Get all rides for a rider with a specific ID
 @app.route("/rider/<int:user_id>/rides", methods=["GET"])
-def get_user_ride_info(user_id: int):
+def get_user_ride_info(user_id:int):
 	try:
 		user_ride_info_tuple = sql.get_list(
 			f"""
@@ -102,13 +129,10 @@ def get_user_ride_info(user_id: int):
 				WHERE user_id = {user_id}
 			"""
 		)
-		if user_ride_info_tuple:
-			user_ride_info = [tuple_to_dict(entry, 'rides') for entry in user_ride_info_tuple]
-			return jsonify(user_ride_info)
-		else:
-			return jsonify({"output": 'No matching records'})
 	except Exception as e:
-		return jsonify({'error':e})
+		return jsonify(e)
+	user_ride_info = [rides_tuple_to_dict(entry) for entry in user_ride_info_tuple]
+	return jsonify(user_ride_info)
 
 # # DELETE /ride/:id
 # Delete a ride with a specific ID
@@ -119,11 +143,17 @@ def delete_ride_info(ride_id:int):
 			f"""
 				DELETE FROM rides
 				WHERE ride_id = {ride_id}
-			"""
-		)
-		return jsonify({'success': f"Ride ID {ride_id} deleted"})
+			""")
+		return jsonify(f'Ride ID {ride_id} deleted')
 	except Exception as e:
-		return jsonify({'error':e})
+		return jsonify(e)
+
+# # GET /daily
+# Get all of the rides in the current day
+@app.route("/daily", methods=["GET"])
+def get_current_day_ride_info():
+
+  return 
 
 # # GET /daily?date=01-01-2020
 # Get all rides for a specific date

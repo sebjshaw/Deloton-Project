d316a3185a457be928a78094cc38e57d53bf9f79
Dominic Lawson <dominic.l12@hotmail.co.uk>
2023-02-02 17:34:09 +0000

    added api endpoint code to fetch data

--- api/api.py
+++ api/api.py
@@ -8,7 +8,7 @@ from datetime import datetime
 dotenv.load_dotenv()
 
 app = Flask(__name__)
-CORS(app, origins=["http://127.0.0.1:8080"],  supports_credentials=True)
+CORS(app, origins=["http://127.0.0.1:8090"],  supports_credentials=True)
 
 USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
@@ -47,6 +47,22 @@ def tuple_to_dict(tup:tuple, table:str) -> dict:
 	for idx, title in enumerate(keys):
 		row_dict[title] = tup[idx]
 	return row_dict
+
+def users_tuple_to_dict(user_info:tuple) -> dict:
+	return {
+		'user_id':user_info[0],
+		'first_name':user_info[1],
+    'last_name':user_info[2],
+    'gender':user_info[3],
+		'address':user_info[4],
+		'date_of_birth':user_info[5],
+		'email_address':user_info[6],
+		'height_cm':user_info[7],
+		'weight_kg':user_info[8],
+		'account_create_date':user_info[9],
+		'bike_serial':user_info[10],
+		'original_source':user_info[11]
+	}
 
 # # GET /ride/:id
 # Get a ride with a specific ID

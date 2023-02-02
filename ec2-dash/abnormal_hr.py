baa2855863313e2aeb62405269eed93424e50fdc
SebShaw <trainee.seb.shaw@sigmalabs.co.uk>
2023-02-02 12:26:53 +0000

    Add code to communicate with SNS topic when abnormal hr detected
    This code now communicates directly with the SNS topic (no need for Lambda) and sends an email warning the user that their heart rate has exceeded their maximum given their age. Some format changes in the dash_sqlite.py to clean the code.

--- ec2-dash/abnormal_hr.py
+++ ec2-dash/abnormal_hr.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 import math
+import json
+from botocore.exceptions import ClientError
+import boto3
 
 TODAY = datetime.now()
 
@@ -31,5 +34,86 @@ def compare_hr_to_max_hr(hr: str, max_hr: int) -> bool:
     
     return False
 
-# initial plan was to send to lambda then the lambda can talk to the SNS but seems 
-# that the EC2 can do this so will talk with the team in the morning 
+def create_JSON_for_email(user_info: dict, curr_hr: int, max_hr:int, date: str, time_elapsed: str):
+
+    new_user_info = {}
+    new_user_info['user_forename'] = user_info['name'].split(" ")[0]
+    new_user_info['user_surname'] = user_info['name'].split(" ")[1]
+    new_user_info['curr_heart_rate'] = curr_hr
+    new_user_info['limit_heart_rate'] = max_hr
+    new_user_info['ride_date'] = date
+    new_user_info['time_elapsed'] = time_elapsed
+    new_user_info['user_email'] = user_info['email_address']
+
+    return new_user_info
+
+def send_user_hr_warning(user_info: dict, curr_hr: int, max_hr: int, date: str, time_elapsed: str):
+
+    user_json = create_JSON_for_email(user_info, curr_hr, max_hr, date, time_elapsed)
+
+    SENDER = "trainee.alex.skowronski@sigmalabs.co.uk"
+    RECIPIENT = "three.musketeers.deloton@gmail.com"
+
+    AWS_REGION = "eu-west-2"
+
+    """The subject line for the email."""
+    SUBJECT = "Heart Rate exceeding normal levels"
+
+    """The email body for recipients with non-HTML email clients."""
+    BODY_TEXT = ("You should be aware that your heart rate is exceeding the normal level of {}\r\n"
+                "This email was sent to you by Deloton."
+                )
+                
+    """The HTML body of the email."""
+    BODY_HTML = f"""<html>
+    <head></head>
+    <body>
+    <h1>Hey {user_json["user_forename"]} {user_json["user_surname"]}, </h1>
+    
+    <h1>This email is to let you know that on {user_json["ride_date"]}, {user_json["time_elapsed"]} seconds into your ride
+     your heart rate reached {user_json["curr_heart_rate"]} bpm, exceeding the typical limit of {user_json["limit_heart_rate"]} bpm.</h1>
+     <h1>Stay safe!</h1>
+     
+    <h2>This email was sent to you by Deloton.</p>
+    </body>
+    </html>
+                """            
+
+    CHARSET = "UTF-8"
+
+    """Create a new SES resource and specify a region."""
+    client = boto3.client('ses',region_name=AWS_REGION)
+
+    """Try to send the email."""
+    try:
+        #Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+        
+                        'Data': BODY_HTML
+                    },
+                    'Text': {
+        
+                        'Data': BODY_TEXT
+                    },
+                },
+                'Subject': {
+
+                    'Data': SUBJECT
+                },
+            },
+            Source=SENDER
+        )
+    # Display an error if something goes wrong.	
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])

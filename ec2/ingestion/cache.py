2d259a3cb65f7da1966b8d9d39773c0cff556b7f
Dominic Lawson <dominic.l12@hotmail.co.uk>
2023-02-06 09:55:59 +0000

    removing comment

--- ec2/ingestion/cache.py
+++ ec2/ingestion/cache.py
@@ -1,7 +1,6 @@
 import redis
 
 redisClient = redis.Redis(host='localhost', port=6379)
-# redis hashes, on for contries and one for institutes
 
 def check_cache_value(email: str) -> bool | None:
 	"""

3e55bdb270ec2660b1be74ad8979353dd10ef811
SebShaw <trainee.seb.shaw@sigmalabs.co.uk>
2023-02-07 17:50:27 +0000

    Format python file into functions

--- daily_report/daily_report.py
+++ daily_report/daily_report.py
@@ -1,6 +1,5 @@
 
-import jinja2
-from jinja2 import Template
+from jinja2 import FileSystemLoader, Environment
 import pdfkit
 
 
@@ -10,14 +9,11 @@ item_2 = 'Avg. Power'
 item_3 = 'Total Ride Time'
 item_4 = 'Total Rides M/F Split'
 item_5 = 'Avg. HR'
-
 item_6 = 'Avg. Ride Duration split by gender'
 item_7 = 'Avg. Ride resistance split by gender'
-
 item_8 = 'x riders went over max heart rate today'
 item_9 = 'x new riders today'
 item_10 = 'The Longest ride today'
-
 footer = 'footer'
 
 context = {
@@ -27,17 +23,37 @@ context = {
     }
 
 
-template_loader = jinja2.FileSystemLoader("./")
-template_env = jinja2.Environment(loader=template_loader)
+def load_html_template() -> str:
+    """Loads the html template from file, adds the graphs and metrics and returns as string
 
-template = template_env.get_template('index.html')
-output_text = template.render(context)
+    Returns:
+        str: the daily_report formatted as a str
+    """
+
+    # create a template loader and template environment using jinja2
+    template_loader = FileSystemLoader("./")
+    template_env = Environment(loader=template_loader)
+
+    # load in the html template, add the graphs and metrics and save as text
+    template = template_env.get_template('index.html')
+    return template.render(context)
+
+
+def create_pdf_output(report_str:str):
+    """Creates a pdf output of the daily report in the working directory 
+
+    Args:
+        report_str (str): the daily report in string format
+    """
+
+    # load text into pdf using pdfkit and wkhtmltopdf and the style.css file as styling
+    config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
+    pdfkit.from_string(report_str, 'pdf_generated.pdf', configuration=config, css='style.css')
 
 
 
-config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
-pdfkit.from_string(output_text, 'pdf_generated.pdf', configuration=config, css='style.css')
 
+output_text = load_html_template()
 
-
+create_pdf_output(output_text)
 

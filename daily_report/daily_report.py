ed05eaccc62fb69f75ca55fba7074d53b2eefc66
SebShaw <trainee.seb.shaw@sigmalabs.co.uk>
2023-02-08 16:00:25 +0000

    Format the template.html. Query RDS for data
    Query the RDS and format the results visually for an appealing daily report. The colour scheme is chosen to match the dashboard for consistency. The deloton logo is also included.

--- daily_report/daily_report.py
+++ daily_report/daily_report.py
@@ -4,6 +4,7 @@ from xhtml2pdf import pisa
 from datetime import datetime
 from PGConnection import SQLConnection as postgres
 import os
+import math
 from visualisations import create_line_graph, create_grouped_bar_graph
 from dotenv import load_dotenv
 load_dotenv()
@@ -39,7 +40,13 @@ def get_total_daily_rides():
     return create_line_graph(df, 'day', 'total_rides')
 
 def get_avg_ride_length_by_gender_and_age():
-	df = pg.get_df(
+    """Gets the average ride length split by user gender and age for last 24 hours. 
+    Returns bar graph visualising the results
+
+    Returns:
+        px.plot: bar graph visualising the result of the query
+    """
+    df = pg.get_df(
 		"""
 			SELECT 
 				u.gender,
@@ -61,7 +68,108 @@ def get_avg_ride_length_by_gender_and_age():
 			ORDER BY age_group;
 		"""
 	)
-	return create_grouped_bar_graph(df, 'age_group', 'average_duration', 'gender', 'Average Ride Duration')
+    return create_grouped_bar_graph(df, 'age_group', 'average_duration', 'gender', 'Average Ride Duration', 's')
+
+def get_avg_power_by_gender_and_age():
+    """Gets the average power length split by user gender and age for las 24 hours. 
+    Returns bar graph visualising the results
+
+    Returns:
+        px.plot: bar graph visualising the result of the query
+    """
+    df = pg.get_df(
+		f"""
+				SELECT 
+					u.gender,
+					CASE
+						WHEN AGE(TO_DATE(u.date_of_birth,'YYYY-MM-DD')) < '18 years' THEN '<18'
+						WHEN AGE(TO_DATE(u.date_of_birth,'YYYY-MM-DD')) BETWEEN '18 years' AND '24 years' THEN '18-24'
+						WHEN AGE(TO_DATE(u.date_of_birth,'YYYY-MM-DD')) BETWEEN '25 years' AND '34 years' THEN '25-34'
+						WHEN AGE(TO_DATE(u.date_of_birth,'YYYY-MM-DD')) BETWEEN '35 years' AND '44 years' THEN '35-44'
+						WHEN AGE(TO_DATE(u.date_of_birth,'YYYY-MM-DD')) BETWEEN '45 years' AND '54 years' THEN '45-54'
+						WHEN AGE(TO_DATE(u.date_of_birth,'YYYY-MM-DD')) BETWEEN '55 years' AND '64 years' THEN '55-64'
+						ELSE '65+'
+					END AS age_group,
+					AVG(r.average_power*r.total_duration) AS average_power
+				FROM users u
+					JOIN rides r
+						USING(user_id)
+				WHERE EXTRACT(EPOCH FROM AGE(NOW(), CAST(CONCAT(r.date, ' ',r.time_started) as TIMESTAMP)))/3600 < 24
+				GROUP BY gender, age_group
+				ORDER BY age_group;
+		"""
+	)
+    return create_grouped_bar_graph(df, 'age_group', 'average_power', 'gender', 'Average Power', 'W')
+
+def get_avg_power():
+    """Gets the average power per ride for the last 24 hours
+
+    Returns:
+        int: average power as an integer
+    """
+    value = pg.get_list(f"""
+            SELECT 
+                AVG(r.average_power*r.total_duration) AS average_power
+            FROM rides AS r
+            WHERE 
+                EXTRACT(EPOCH FROM AGE(NOW(), CAST(CONCAT(r.date, ' ',r.time_started) as TIMESTAMP)))/3600 < 24
+            """)[-1][0]
+    return value 
+
+def get_total_ride_time(gender:str):
+    """Gets the total ride time in the last 24 hours. Can be split by gender
+    Args:
+        gender (str): The gender which the function will calculate the total ride time for 
+    Returns:
+        int: total ride time as an integer
+    """
+    if gender == 'both':
+        value = pg.get_list(f"""
+                SELECT 
+                    SUM(r.total_duration) AS total_ride_time
+                FROM rides AS r
+                WHERE 
+                    EXTRACT(EPOCH FROM AGE(NOW(), CAST(CONCAT(r.date, ' ',r.time_started) as TIMESTAMP)))/3600 < 24
+                """)[-1][0]
+    else:
+        value = pg.get_list(f"""
+                SELECT 
+                    SUM(r.total_duration) AS total_ride_time
+                FROM users u
+					JOIN rides r
+						USING(user_id)
+                WHERE 
+                    (EXTRACT(EPOCH FROM AGE(NOW(), CAST(CONCAT(r.date, ' ',r.time_started) as TIMESTAMP)))/3600 < 24) AND (u.gender = '{gender}')
+                """)[-1][0]
+    return value
+
+def get_avg_hr():
+    """Gets the average heart rate per ride for the last 24 hours
+
+    Returns:
+        int: average heart rate as an integer
+    """
+    value = pg.get_list(f"""
+            SELECT 
+                AVG(r.average_heart_rate) AS average_hr
+            FROM rides AS r
+            WHERE 
+                EXTRACT(EPOCH FROM AGE(NOW(), CAST(CONCAT(r.date, ' ',r.time_started) as TIMESTAMP)))/3600 < 24
+            """)[-1][0]
+    return value 
+
+def get_longest_ride():
+    value = pg.get_list(f"""
+            SELECT 
+                total_duration
+            FROM rides AS r
+            WHERE 
+                EXTRACT(EPOCH FROM AGE(NOW(), CAST(CONCAT(r.date, ' ',r.time_started) as TIMESTAMP)))/3600 < 24
+            ORDER BY 
+                total_duration DESC
+            LIMIT 1
+            """)[-1][0]
+    return value
 
 def figure_to_base64(figure):
 
@@ -70,32 +178,31 @@ def figure_to_base64(figure):
  
     return image_html
 
-
-
-
 today = str(datetime.now().date())
 total_rides = get_total_daily_rides()
 total_rides_html = figure_to_base64(total_rides)
-item2 = 'Avg. Power'
-item3 = 'Total Ride Time'
-item4 = 'Total Rides M/F Split'
-item5 = 'Avg. HR'
+avg_power = f"{(math.floor(get_avg_power()))} W"
+total_ride_time = f"{get_total_ride_time('both')} s"
+ride_time_m = f"{get_total_ride_time('Male')} s"
+ride_time_f = f"{get_total_ride_time('Female')} s"
+avg_hr = f"{math.floor(get_avg_hr())} bpm"
 avg_ride_dur = get_avg_ride_length_by_gender_and_age()
 avg_ride_dur_html = figure_to_base64(avg_ride_dur)
-item7 = 'Avg. Ride resistance split by gender'
-item8 = 'x riders went over max heart rate today'
-item9 = 'x new riders today'
-item10 = 'The Longest ride today'
+avg_ride_pow = get_avg_power_by_gender_and_age()
+avg_ride_pow_html = figure_to_base64(avg_ride_pow)
+over_max_hr = 'x riders went over max heart rate today'
+new_riders = 'x new riders today'
+longest_ride = f"{get_longest_ride()} s"
 logo = 'logo'
 authors = 'authors'
 
 content_dict = {
-    '{{ date }}':today,'{{ item1 }}':total_rides_html,
-    '{{ item2 }}':item2,'{{ item3 }}':item3,
-    '{{ item4 }}':item4,'{{ item5 }}':item5,
-    '{{ item6 }}':avg_ride_dur_html,'{{ item7 }}':item7,
-    '{{ item8 }}':item8,'{{ item9 }}':item9,
-    '{{ item10 }}':item10,}
+    '{{ date }}':today,'{{ total_rides_graph }}':total_rides_html,
+    '{{ avg_power }}':avg_power,'{{ total_ride_time }}':total_ride_time,
+    '{{ ride_time_m }}': ride_time_m, '{{ ride_time_f }}': ride_time_f, '{{ avg_hr }}':avg_hr,
+    '{{ avg_dur_graph }}':avg_ride_dur_html,'{{ avg_pow_graph }}':avg_ride_pow_html,
+    '{{ over_max_hr }}':over_max_hr,'{{ new_riders }}':new_riders,
+    '{{ longest_ride }}':longest_ride}
 
 def create_html_report(template_file, content_dict):
     with open(template_file, 'r') as f:
--- daily_report/visualisations.py
+++ daily_report/visualisations.py
@@ -11,15 +11,19 @@ def create_line_graph(df: pd.DataFrame, x: str, y:str) -> px.line:
 			df (pd.DataFrame): dataframe containing columns to be plotted
 			x (str): string of column name
 			y (str): string of column name
+			units (str): the units to go on the axis
 
 	Returns:
 			px.line: line graph plotting x and y
 	"""
+	y_title = y.replace("_"," ").capitalize()
+	x_title = x.replace("_"," ").capitalize()
+
 	fig = px.line(df, x, y).update_layout(paper_bgcolor="#333333", plot_bgcolor='#333333')
 
-	fig.update_xaxes(title_font=dict(color='#f3dfc1'), tickfont=dict(color='#f3dfc1'))
+	fig.update_xaxes(title = x_title, title_font=dict(color='#f3dfc1'), tickfont=dict(color='#f3dfc1'))
 	fig.update_xaxes(gridcolor='#7fc37e', zerolinecolor='#7fc37e', zerolinewidth=3)
-	fig.update_yaxes(title_font=dict(color='#f3dfc1'), tickfont=dict(color='#f3dfc1'))
+	fig.update_yaxes(title=y_title, title_font=dict(color='#f3dfc1'), tickfont=dict(color='#f3dfc1'))
 	fig.update_yaxes(gridcolor='#7fc37e', zerolinecolor='#7fc37e', zerolinewidth=3)
 	fig.update_layout(
     legend=dict(
@@ -28,10 +32,10 @@ def create_line_graph(df: pd.DataFrame, x: str, y:str) -> px.line:
         )
     )
 	)
-	
+
 	return fig
 
-def create_grouped_bar_graph(df:pd.DataFrame, x:str, y:str, group:str, title: str) -> px.bar:
+def create_grouped_bar_graph(df:pd.DataFrame, x:str, y:str, group:str, title: str, units:str) -> px.bar:
 	"""
 	Create a bar graph visualisation for the dataframe passed in the function
 
@@ -41,16 +45,20 @@ def create_grouped_bar_graph(df:pd.DataFrame, x:str, y:str, group:str, title: st
 			y (str): variable to be plotted in y
 			group (str): group by column
 			title (str): graph title
+			units (str): the units to be displayed on the y axis
 
 	Returns:
 			px.bar: bar graph
 	"""
+	y_title = y.replace("_"," ").capitalize()
+	x_title = x.replace("_"," ").capitalize()
+
 	fig = px.bar(df, x, y, color=group, barmode='group', text_auto='.0f', title=title).update_layout(paper_bgcolor="#333333", plot_bgcolor='#333333')
 
 	fig.update_xaxes(title_font=dict(color='#f3dfc1'), tickfont=dict(color='#f3dfc1'))
-	fig.update_xaxes(gridcolor='#7fc37e', zerolinecolor='#7fc37e', zerolinewidth=3)
+	fig.update_xaxes(gridcolor='#7fc37e', zerolinecolor='#7fc37e', zerolinewidth=3, title=x_title)
 	fig.update_yaxes(title_font=dict(color='#f3dfc1'), tickfont=dict(color='#f3dfc1'))
-	fig.update_yaxes(gridcolor='#7fc37e', zerolinecolor='#7fc37e', zerolinewidth=3)
+	fig.update_yaxes(gridcolor='#7fc37e', zerolinecolor='#7fc37e', zerolinewidth=3, title=f"{y_title} / {units}")
 	fig.update_layout(
     legend=dict(
         font=dict(

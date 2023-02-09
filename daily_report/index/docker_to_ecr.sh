9deadacbe62415415ed0161aac1e800e1ce42923
Dominic Lawson <dominic.l12@hotmail.co.uk>
2023-02-09 14:17:39 +0000

    index page finalise

--- daily_report/index/docker_to_ecr.sh
+++ daily_report/index/docker_to_ecr.sh
@@ -1,4 +1,4 @@
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 605126261673.dkr.ecr.eu-west-2.amazonaws.com
-docker tag 6d24945441bd 605126261673.dkr.ecr.eu-west-2.amazonaws.com/three-musketeers:dr-index-page-build
+docker tag 2a1077666143 605126261673.dkr.ecr.eu-west-2.amazonaws.com/three-musketeers:dr-index-page-build
 docker push 605126261673.dkr.ecr.eu-west-2.amazonaws.com/three-musketeers:dr-index-page-build
 aws lambda update-function-code --function-name Three-M-report-index --image-uri 605126261673.dkr.ecr.eu-west-2.amazonaws.com/three-musketeers:dr-index-page-build

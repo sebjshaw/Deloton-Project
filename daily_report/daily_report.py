6584ee8dcfcfd0a82f439f18f7688c61971f8cb8
SebShaw <trainee.seb.shaw@sigmalabs.co.uk>
2023-02-09 09:56:49 +0000

    Further changes to the format. Encoding logo png as base64
    Encoded the png logo as base64 to insert into the template html in a similar fashion to the plotly graphs

--- daily_report/daily_report.py
+++ daily_report/daily_report.py
@@ -3,7 +3,9 @@ from datetime import datetime
 from PGConnection import SQLConnection as postgres
 import os
 import math
+from typing import Union 
 import s3fs
+import plotly.express as px
 from visualisations import create_line_graph, create_grouped_bar_graph
 from dotenv import load_dotenv
 load_dotenv()
@@ -19,7 +21,7 @@ DB_NAME = os.getenv('DB_NAME')
 
 pg = postgres(USERNAME, PASSWORD, HOST, PORT, DB_NAME)
 
-def pull_txt_file_from_s3() -> object:
+def pull_txt_file_from_s3() -> str:
     """Pulls the txt file from the bucket that contains the number of hr warning emails that were sent in the last 24 hours
 
     Returns:
@@ -34,7 +36,7 @@ def pull_txt_file_from_s3() -> object:
 
     return total_emails
 
-def get_total_daily_rides():
+def get_total_daily_rides() -> px.line:
     """Creates a line graph tracking the total rides over the last 7 days 
 
     Returns:
@@ -54,12 +56,12 @@ def get_total_daily_rides():
 
     return create_line_graph(df, 'day', 'total_rides')
 
-def get_avg_ride_length_by_gender_and_age():
+def get_avg_ride_length_by_gender_and_age() -> px.bar:
     """Gets the average ride length split by user gender and age for last 24 hours. 
     Returns bar graph visualising the results
 
     Returns:
-        px.plot: bar graph visualising the result of the query
+        px.bar: bar graph visualising the result of the query
     """
     df = pg.get_df(
 		"""
@@ -85,12 +87,12 @@ def get_avg_ride_length_by_gender_and_age():
 	)
     return create_grouped_bar_graph(df, 'age_group', 'average_duration', 'gender', 'Average Ride Duration', 's')
 
-def get_avg_power_by_gender_and_age():
+def get_avg_power_by_gender_and_age() -> px.bar:
     """Gets the average power length split by user gender and age for las 24 hours. 
     Returns bar graph visualising the results
 
     Returns:
-        px.plot: bar graph visualising the result of the query
+        px.bar: bar graph visualising the result of the query
     """
     df = pg.get_df(
 		f"""
@@ -116,7 +118,7 @@ def get_avg_power_by_gender_and_age():
 	)
     return create_grouped_bar_graph(df, 'age_group', 'average_power', 'gender', 'Average Power', 'W')
 
-def get_avg_power():
+def get_avg_power() -> int:
     """Gets the average power per ride for the last 24 hours
 
     Returns:
@@ -131,7 +133,7 @@ def get_avg_power():
             """)[-1][0]
     return value 
 
-def get_total_ride_time(gender:str):
+def get_total_ride_time(gender:str) -> int:
     """Gets the total ride time in the last 24 hours. Can be split by gender
     Args:
         gender (str): The gender which the function will calculate the total ride time for 
@@ -158,7 +160,7 @@ def get_total_ride_time(gender:str):
                 """)[-1][0]
     return value
 
-def get_avg_hr():
+def get_avg_hr() -> int:
     """Gets the average heart rate per ride for the last 24 hours
 
     Returns:
@@ -173,7 +175,7 @@ def get_avg_hr():
             """)[-1][0]
     return value 
 
-def get_longest_ride():
+def get_longest_ride() -> int:
     value = pg.get_list(f"""
             SELECT 
                 total_duration
@@ -186,7 +188,7 @@ def get_longest_ride():
             """)[-1][0]
     return value
 
-def get_largest_power():
+def get_largest_power() -> int:
     value = pg.get_list(f"""
             SELECT 
                 max_power
@@ -199,37 +201,34 @@ def get_largest_power():
             """)[-1][0]
     return value
 
-def figure_to_base64(figure):
+def encode_logo_as_base64() -> str:
+    """Takes the logo png and encodes as base64 for insertion into the html template
+
+    Returns:
+        str: base64 encoded logo.png
+    """
+
+    with open("deleton_logo.png", "rb") as image_file:
+        encoded_string = str(base64.b64encode(image_file.read()))[2:-1]
+
+    logo_html = (f'<img class="figure" src="data:image/png;base64,{encoded_string}"><br>')
+
+    return logo_html
+
+def figure_to_base64(figure: Union[px.bar, px.line]) -> str:
+    """Takes a plotly graph and encodes a png version as base64 to be inserted into the html template
+
+    Args:
+        figure (px.plot): either a plotly bar or line chart  
+
+    Returns:
+        str: base64 encoded png version of the plotly graph
+    """
 
     image = str(base64.b64encode(figure.to_image(format="png", scale=2)))[2:-1]
     image_html = (f'<img class="figure" src="data:image/png;base64,{image}"><br>')
  
     return image_html
-
-
-today = str(datetime.now().date())
-total_rides = get_total_daily_rides()
-total_rides_html = figure_to_base64(total_rides)
-avg_power = f"{(round(get_avg_power(), 2))} W"
-total_ride_time = f"{get_total_ride_time('both')} s"
-ride_time_m = f"{get_total_ride_time('Male')} s"
-ride_time_f = f"{get_total_ride_time('Female')} s"
-avg_hr = f"{math.floor(get_avg_hr())} bpm"
-avg_ride_dur = get_avg_ride_length_by_gender_and_age()
-avg_ride_dur_html = figure_to_base64(avg_ride_dur)
-avg_ride_pow = get_avg_power_by_gender_and_age()
-avg_ride_pow_html = figure_to_base64(avg_ride_pow)
-over_max_hr = pull_txt_file_from_s3()
-max_power = f"{round(get_largest_power(), 2)} W"
-longest_ride = f"{get_longest_ride()} s"
-
-content_dict = {
-    '{{ date }}':today,'{{ total_rides_graph }}':total_rides_html,
-    '{{ avg_power }}':avg_power,'{{ total_ride_time }}':total_ride_time,
-    '{{ ride_time_m }}': ride_time_m, '{{ ride_time_f }}': ride_time_f, '{{ avg_hr }}':avg_hr,
-    '{{ avg_dur_graph }}':avg_ride_dur_html,'{{ avg_pow_graph }}':avg_ride_pow_html,
-    '{{ over_max_hr }}':over_max_hr,'{{ max_power }}':max_power,
-    '{{ longest_ride }}':longest_ride}
 
 def create_html_report(template_file, content_dict):
     with open(template_file, 'r') as f:
@@ -240,12 +239,39 @@ def create_html_report(template_file, content_dict):
         report_html = report_html.replace(content, content_dict[content])
         
 
-    
     return report_html
  
  
-report_html = create_html_report("template.html", content_dict)
+def lambda_handler(event,context):
+    today = str(datetime.now().date())
+    total_rides = get_total_daily_rides()
+    total_rides_html = figure_to_base64(total_rides)
+    avg_power = f"{(round(get_avg_power(), 2))} W"
+    total_ride_time = f"{get_total_ride_time('both')} s"
+    ride_time_m = f"{get_total_ride_time('Male')} s"
+    ride_time_f = f"{get_total_ride_time('Female')} s"
+    avg_hr = f"{math.floor(get_avg_hr())} bpm"
+    avg_ride_dur = get_avg_ride_length_by_gender_and_age()
+    avg_ride_dur_html = figure_to_base64(avg_ride_dur)
+    avg_ride_pow = get_avg_power_by_gender_and_age()
+    avg_ride_pow_html = figure_to_base64(avg_ride_pow)
+    over_max_hr = pull_txt_file_from_s3()
+    max_power = f"{round(get_largest_power(), 2)} W"
+    longest_ride = f"{get_longest_ride()} s"
+    logo_html = encode_logo_as_base64()
 
-with open('todays_report.html', 'w') as f:
-    f.write(report_html)
- 
+    content_dict = {
+        '{{ date }}':today,'{{ total_rides_graph }}':total_rides_html,
+        '{{ avg_power }}':avg_power,'{{ total_ride_time }}':total_ride_time,
+        '{{ ride_time_m }}': ride_time_m, '{{ ride_time_f }}': ride_time_f, '{{ avg_hr }}':avg_hr,
+        '{{ avg_dur_graph }}':avg_ride_dur_html,'{{ avg_pow_graph }}':avg_ride_pow_html,
+        '{{ over_max_hr }}':over_max_hr,'{{ max_power }}':max_power,
+        '{{ longest_ride }}':longest_ride, '{{ logo }}': logo_html}
+
+    report_html = create_html_report("template.html", content_dict)
+
+    with open(f'{today}.html', 'w') as f:
+        f.write(report_html)
+
+
+lambda_handler('x', 'x')
